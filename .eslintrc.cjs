a02044819abc25ffb71133e53883c59f3c0aba58
YoanBor <yoanbor@icloud.com>
2024-05-28 21:03:28 +0200

    build(projet): Ajout de prettier, eslint et husky au projet

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb/hooks',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
