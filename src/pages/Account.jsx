aca9a63e7bb251734ea40027064ce4aaf066a64d
YoanBor <yoanbor@icloud.com>
2024-05-29 16:51:28 +0200

    reload project to 0

--- src/pages/Account.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Account = () => {
-  return <h1>Compte</h1>;
-};
-
-export default Account;
--- src/pages/Register.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Register = () => {
-  return <h1>hoz</h1>;
-};
-
-export default Register;
