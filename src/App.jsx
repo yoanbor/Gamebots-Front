bb31158839798487d16c9b12bb27340f923c11c1
YoanBor <yoanbor@icloud.com>
2024-07-03 15:51:05 +0200

    Ajout de react-router-dom, modification de main.jsx pour le style, création du dossier style et modification de App.jsx pour accueillir un router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PageError from "./PageError.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Home from "./pages/Home.jsx";
+import Account from "./pages/Account.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <PageError/>
+    },
+    {
+        path: "/home",
+        element: <Home />,
+        errorElement: <PageError/>
+    },
+    {
+        path: "/login",
+        element: <Login />,
+        errorElement: <PageError/>
+    },
+    {
+        path: "/register",
+        element: <Register />,
+        errorElement: <PageError/>
+    },
+    {
+        path: 'account',
+        element: <Account/>,
+    }
+    ]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return <RouterProvider router={router}/>;
 }
 
-export default App
+export default App;
