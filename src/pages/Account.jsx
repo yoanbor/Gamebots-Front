e0fe6026420a0cf8f005441c3eed2a2b6a182b16
YoanBor <yoanbor@icloud.com>
2024-07-13 04:51:00 +0200

    feat(pages): Ajout de la page Home et Account qui retourneront le header

---
+++ src/pages/Account.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header.jsx";
+
+const Account = () => {
+    return (
+        <Header />
+    );
+};
+
+export default Account;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header.jsx";
+
+const Home = () => {
+    return (
+        <Header />
+    );
+};
+
+export default Home;
