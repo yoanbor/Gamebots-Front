bf21aaec66c7c33739694430d929698d3110df84
YoanBor <yoanbor@icloud.com>
2024-07-14 01:44:16 +0200

    refactor, style(Login, Register): Création d'un composant pour la left part qui est la même sur ces deux pages et de la feuille séparée pour ce style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { useState, useRef } from "react";
+import RegisterAndLoginLeftPart from "../components/RegisterLoginLeftPart.jsx";
 
 function Login() {
     const [inputType, setInputType] = useState('password');
@@ -15,33 +16,7 @@ function Login() {
     return (
         <div className={"login"}>
 
-            <div className="login-left-part">
-                <img src={"../../public/avatars/AvatarBang.png"} id={"avatar-Bangalore"} className={"img-avatars"} alt="Avatar Bangalor" />
-                <img src={"../../public/avatars/AvatarBlood.png"} id={"avatar-Bloodhunt"} className={"img-avatars"} alt="Avatar Bloodhunt" />
-                <img src={"../../public/avatars/AvatarLifeline.png"} id={"avatar-Lifeline"} className={"img-avatars"} alt="Avatar Lifeline" />
-                <img src={"../../public/avatars/AvatarPath.png"} id={"avatar-Pathfinder"} className={"img-avatars"} alt="Avatar Pathfinder" />
-                <img src={'../../public/Logo.svg'} id={"big-logo"} className={"img-avatars"} alt="logo Gamebots" />
-
-                <div className={'msg'} id={"msg-gamebot"}>
-                    <div className={"img-gamebot"}>
-                        <img src={'../../public/Logo.svg'} id={"logo-msg"} alt="logo Gamebots" />
-                    </div>
-                    <div className={"message-gamebot"}>
-                        <h4>Gamebot</h4>
-                        <p>Bonjour, en quoi puis-je vous aider ?</p>
-                    </div>
-                </div>
-
-                <div className={"msg"} id={"msg-user"}>
-                    <div className={"message-user"}>
-                        <h4>Herobrine</h4>
-                        <p>Comment économiser ses munitions ?</p>
-                    </div>
-                    <div className={"img-user"}>
-                        <img src={"../../public/avatars/AvatarPath.png"} id={"logo-msg"} alt="Avatar Pathflinder" />
-                    </div>
-                </div>
-            </div>
+            <RegisterAndLoginLeftPart />
 
             <div className="login-right-part">
                 <h1>Gamebots</h1>
