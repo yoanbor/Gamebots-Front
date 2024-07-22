99039d7c653f3ee9f4db936a23b999ea0b09584f
YoanBor <yoanbor@icloud.com>
2024-07-22 18:54:14 +0200

    fix(service AuthService): les requêtes axios prennent en compte la gestion des erreurs

--- src/services/token/AuthService.jsx
+++ src/services/token/AuthService.jsx
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-function loginUser(user) {
-    return axios.post('http://localhost:8080/login', user);
-}
+const loginUser = async (user) => {
+    try {
+        const response = await axios.post('http://localhost:8080/login', user, {
+            method: 'POST'
+        });
+        return response.data;
+    } catch (error) {
+        console.log("Erreur lors de la connexion de l'utilisateur");
+        throw error;
+    }
+};
 
 export default loginUser;
