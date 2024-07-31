fe25d40788816dce12cbf00bdf0cf4bdee8514c3
YoanBor <yoanbor@icloud.com>
2024-07-31 05:24:54 +0200

    feat(GetAvatarImagesService, GetAvatarImagesController.jsx): Ajout du service et du controller pour récupérer toutes les images de type avatar

---
+++ src/controllers/image/GetAvatarImagesController.jsx
@@ -0,0 +1,12 @@
+import getAvatarImages from "../../services/image/GetAvatarImagesService.jsx";
+
+const getAvatarImagesController = async () => {
+    try {
+        return getAvatarImages();
+    } catch (error) {
+        console.log("Erreur lors de la récupération des images de type Avatar");
+        throw error;
+    }
+};
+
+export default getAvatarImagesController;
---
+++ src/services/image/GetAvatarImagesService.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const getAvatarImages = async () => {
+    const token = localStorage.getItem("token");
+
+    try {
+        const response = await axios.get(
+            'http://localhost:8080/images/avatars', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+        return response.data;
+    } catch (error) {
+        console.log("Erreur lors de la récupération des images de type Avatar");
+        throw error;
+    }
+};
+
+export default getAvatarImages;
