7c4fb2bc79c26e66545c851168450529c6bbf4e1
YoanBor <yoanbor@icloud.com>
2024-08-02 07:47:47 +0200

    feat(handleSubmitForAccount): ajout de la gestion de la soumission pour la modification du compte utilisateur

--- src/business/account/handleSubmitForAccount.jsx
+++ src/business/account/handleSubmitForAccount.jsx
@@ -1,28 +1,48 @@
-import getUserAccountByUserIdController from "../../controllers/user/GetUserAccountByUserIdController.jsx";
-import User from "../../models/User.jsx";
 import modifyUserAccountController from "../../controllers/user/ModifyUserAccountController.jsx";
+import getUserAccountByUserIdController from "../../controllers/user/GetUserAccountByUserIdController.jsx";
 
-export const handleSubmitForAccount = async (e, userAccountId, password, userId, pseudo, phoneNumber, email, setError) => {
+export const handleSubmitForAccount = async (
+    e,
+    userAccountId,
+    password,
+    userId,
+    pseudo,
+    phoneNumber,
+    email,
+    setError,
+    imageId
+) => {
     e.preventDefault();
+
+    console.log(e, userAccountId, password, userId, pseudo, phoneNumber, email, setError, imageId);
 
     try {
         const currentUser = await getUserAccountByUserIdController(userAccountId);
-        const updatedPassword = password ? password : currentUser.password;
 
-        const updatedUser = new User(
-            userId,
-            pseudo,
-            phoneNumber,
-            email,
-            updatedPassword,
-            currentUser.creationDate,
-            new Date(),
-            currentUser.image
-        );
+        const finalPassword = password && password.trim() !== "" ? password : null;
+
+        const updatedUser = {
+            idUser: userId,
+            username: pseudo,
+            phone: phoneNumber,
+            email: email,
+            password: finalPassword,
+            creationDate: currentUser.creationDate,
+            modificationDate: new Date(),
+            imageId: imageId
+        };
+
+        console.log(updatedUser);
 
         await modifyUserAccountController(updatedUser);
         setError('Modification réussie');
+
+        const avatarUrl = updatedUser.imageId ? `/avatars/${updatedUser.imageId}.png` : '/avatars/defaultAvatar.png';
+        localStorage.setItem('userAvatar', avatarUrl);
+        window.dispatchEvent(new Event('avatarUpdated'));
+
     } catch (error) {
+        console.error("Erreur lors de la modification de l'utilisateur", error);
         setError("Erreur lors de la modification de l'utilisateur");
     }
 };
