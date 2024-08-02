3a1cddb83219a3221a452606d956e378db59f238
YoanBor <yoanbor@icloud.com>
2024-08-02 09:15:33 +0200

    fix(fetchGameData): Utilisation de la bonne requête axios pour avoir les images de type banner

--- src/business/home/fetchGameData.jsx
+++ src/business/home/fetchGameData.jsx
@@ -1,17 +1,11 @@
 import getAllGamesController from "../../controllers/game/GetAllGamesController";
-import getAllImages from "../../services/image/GetAllImagesService.jsx";
+import getImageBannerByGameIdController from "../../controllers/image/GetImageBannerByGameIdController";
 import Game from "../../models/Game";
 import Image from "../../models/Image";
 
 export const fetchGameData = async (setData, setError) => {
     try {
         const result = await getAllGamesController();
-        const imagesData = await getAllImages();
-
-        const imagesByGameId = imagesData.reduce((acc, image) => {
-            acc[image.game.idGame] = image;
-            return acc;
-        }, {});
 
         const games = await Promise.all(result.map(async (gameData) => {
             const game = new Game(
@@ -27,17 +21,22 @@ export const fetchGameData = async (setData, setError) => {
                 gameData.modificationDate
             );
 
-            const imageData = imagesByGameId[game.idGame];
-            if (imageData) {
-                game.bannerImage = new Image(
-                    imageData.idImage,
-                    imageData.name,
-                    imageData.source,
-                    imageData.creationDate,
-                    imageData.modificationDate,
-                    imageData.game,
-                    imageData.typeImage
-                );
+            try {
+                const images = await getImageBannerByGameIdController(game.idGame);
+                if (images && images.length > 0) {
+                    const imageData = images[0];
+                    game.bannerImage = new Image(
+                        imageData.idImage,
+                        imageData.name,
+                        imageData.source,
+                        imageData.creationDate,
+                        imageData.modificationDate,
+                        imageData.game,
+                        imageData.typeImage
+                    );
+                }
+            } catch (error) {
+                console.error(`Erreur lors de la récupération de la bannière pour le jeu ${game.title}`, error);
             }
 
             return game;
